837b578ad3052440dca5f2b5512a5f98078388a6
yurimalheiros <yurimalheiros@gmail.com>
2018-10-12 22:05:27 -0300

    adiciona de volta o pdf sobre aprendizagem de máquina

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-cd resolucao-de-problemas
-pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  resolucao-de-problemas.md --pdf-engine=pdflatex -o resolucao-de-problemas.pdf
-cd ..
-
-cd planejamento
-pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  planejamento.md --pdf-engine=pdflatex -o planejamento.pdf
-cd ..
-
-cd arvores-de-decisao
-pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  arvoresdedecisao.md --pdf-engine=pdflatex -o arvoresdedecisao.pdf
-cd ..
+# cd resolucao-de-problemas
+# pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  resolucao-de-problemas.md --pdf-engine=pdflatex -o resolucao-de-problemas.pdf
+# cd ..
+#
+# cd planejamento
+# pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  planejamento.md --pdf-engine=pdflatex -o planejamento.pdf
+# cd ..
+#
+# cd arvores-de-decisao
+# pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  arvoresdedecisao.md --pdf-engine=pdflatex -o arvoresdedecisao.pdf
+# cd ..
 
 cd aprendizagem-de-maquina
 pandoc -f markdown+link_attributes+footnotes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  aprendizagemdemaquina.md --pdf-engine=pdflatex -o aprendizagemdemaquina.pdf
 cd ..
 
-cd aprendizagem-de-maquina2
-pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  aprendizagemdemaquina.md --pdf-engine=pdflatex -o aprendizagemdemaquina.pdf
-cd ..
-
-cd algoritmos-geneticos
-pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  algoritmosgeneticos.md --pdf-engine=pdflatex -o algoritmosgeneticos.pdf
-cd ..
+# cd aprendizagem-de-maquina2
+# pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  aprendizagemdemaquina.md --pdf-engine=pdflatex -o aprendizagemdemaquina.pdf
+# cd ..
+#
+# cd algoritmos-geneticos
+# pandoc -f markdown+link_attributes --highlight-style=tango -V lang=pt-BR --template=../ianotes.latex  algoritmosgeneticos.md --pdf-engine=pdflatex -o algoritmosgeneticos.pdf
+# cd ..
